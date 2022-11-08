e23901d0527ae99367d6ef1e514bf9988447c681
κακαšhï hα†ακε ☯️ <88052431+Sparky9992@users.noreply.github.com>
2022-11-08 19:18:50 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,12 +3,12 @@ const chalk = require("chalk");
 require("dotenv").config();
 let gg = process.env.MODS;
 if (!gg) {
-  gg = "917003213983, 923087880256";
+  gg = "918839439805 , 923190825808";
 }
 
 global.owner = gg.split(",");
-global.mongodb = process.env.MONGODB || "none";
-global.prefa = process.env.PREFIX || "."
+global.mongodb = process.env.MONGODB || "mongodb+srv://carabot9:ishaan09@cluster0.wbn9l.mongodb.net/?retryWrites=true&w=majority";
+global.prefa = process.env.PREFIX || ":"
 global.ggle = process.env.GOOGLE_API || "none"
 global.wter = process.env.WEATHER_API || "none"
 global.pgdb = process.env.DATABASE_URL || "none";
